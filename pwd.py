de1d6af1a96143123a5177a4b0a9fca15e467d37
rvieira <ricardo@rvieira.co.uk>
2021-11-04 15:25:29 +0100

    Initial pwd.py 	new file:   pwd.py

---
+++ pwd.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+
+# generates password with letters, digits and special symbols
+# Author: Ricardo Vieira
+# Creation date: 18/01/2021
+
+import secrets
+import string
+
+def compliant_pwd(candidate_pwd):
+    check_upper = check_lower = check_special_char = False
+    
+#    print('try --> '+candidate_pwd)
+    for letter in candidate_pwd:
+        if letter.isupper():
+            check_upper = True
+        if letter.islower():
+            check_lower = True
+        if letter in ('._+-'):
+            check_special_char = True
+    return check_lower and check_upper and check_special_char
+        
+password_length=20
+special_characters = '._+-'
+password = ''
+alphabet = string.ascii_letters + string.digits + special_characters
+while not compliant_pwd(password):
+    password = ''.join(secrets.choice(alphabet) for i in range(password_length))
+print(password)
