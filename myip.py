a845ae110443564accb1f4eed65b08e906de12ef
rvieira <ricardo@rvieira.co.uk>
2022-07-24 20:08:03 +0200

    Made myip.py executable

--- myip.py
+++ myip.py
@@ -1,2 +1,5 @@
-import os 
-import urllib2
+#!/usr/bin/env python
+
+from requests import get
+ip = get('https://api.ipify.org').content.decode('utf8')
+print('My public IP address is: {}'.format(ip))
